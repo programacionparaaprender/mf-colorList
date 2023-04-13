19aba5d3e548f0693c0f4d4679997bd22a31c45b
programacionparaaprender <yancel209@gmail.com>
2023-04-12 20:06:14 -0500

    agregando actualización

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ https://sweetalert2.github.io/v10.html
 
 ### comando
 yarn add sweetalert2
+
+
+### comando no funciona
+gc "dev mode"
--- webpack.config.js
+++ webpack.config.js
@@ -3,65 +3,68 @@ const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPl
 
 const deps = require("./package.json").dependencies;
 module.exports = {
-  output: {
-    //publicPath: "http://localhost:3002/",
-    publicPath: "https://fantastic-mf-colorlist.netlify.app/",
-  },
 
-  resolve: {
-    extensions: [".tsx", ".ts", ".jsx", ".js", ".json"],
-  },
-
-  devServer: {
-    port: 3002,
-    historyApiFallback: true,
-  },
-
-  module: {
-    rules: [
-      {
-        test: /\.m?js/,
-        type: "javascript/auto",
-        resolve: {
-          fullySpecified: false,
+    output: {
+      publicPath: argv.mode == "development" ? "http://localhost:3002/" : "https://fantastic-mf-colorlist.netlify.app/",
+      //publicPath: "http://localhost:3002/",
+      //publicPath: "https://fantastic-mf-colorlist.netlify.app/",
+    },
+  
+    resolve: {
+      extensions: [".tsx", ".ts", ".jsx", ".js", ".json"],
+    },
+  
+    devServer: {
+      port: 3002,
+      historyApiFallback: true,
+    },
+  
+    module: {
+      rules: [
+        {
+          test: /\.m?js/,
+          type: "javascript/auto",
+          resolve: {
+            fullySpecified: false,
+          },
         },
-      },
-      {
-        test: /\.(css|s[ac]ss)$/i,
-        use: ["style-loader", "css-loader", "postcss-loader"],
-      },
-      {
-        test: /\.(ts|tsx|js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
+        {
+          test: /\.(css|s[ac]ss)$/i,
+          use: ["style-loader", "css-loader", "postcss-loader"],
         },
-      },
+        {
+          test: /\.(ts|tsx|js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+          },
+        },
+      ],
+    },
+  
+    plugins: [
+      new ModuleFederationPlugin({
+        name: "mf_colorlist",
+        filename: "remoteEntry.js",
+        remotes: {},
+        exposes: {
+          "./ColorList": "./src/components/ColorList.jsx"
+        },
+        shared: {
+          ...deps,
+          react: {
+            singleton: true,
+            requiredVersion: deps.react,
+          },
+          "react-dom": {
+            singleton: true,
+            requiredVersion: deps["react-dom"],
+          },
+        },
+      }),
+      new HtmlWebPackPlugin({
+        template: "./src/index.html",
+      }),
     ],
-  },
 
-  plugins: [
-    new ModuleFederationPlugin({
-      name: "mf_colorlist",
-      filename: "remoteEntry.js",
-      remotes: {},
-      exposes: {
-        "./ColorList": "./src/components/ColorList.jsx"
-      },
-      shared: {
-        ...deps,
-        react: {
-          singleton: true,
-          requiredVersion: deps.react,
-        },
-        "react-dom": {
-          singleton: true,
-          requiredVersion: deps["react-dom"],
-        },
-      },
-    }),
-    new HtmlWebPackPlugin({
-      template: "./src/index.html",
-    }),
-  ],
-};
+}
